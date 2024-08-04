60878a648a09093801358d6006b3b5b5e6a0560f
Zakir Mustafa Kemal Kalayci <simurgientest@gmail.com>
2024-08-04 19:22:44 +0300

    update screen resolution

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    viewportWidth: 1920,
-    viewportHeight: 1080,
+    viewportWidth: 2560,
+    viewportHeight: 1440,
     defaultCommandTimeout: 120000,
     requestTimeout: 120000,
     responseTimeout: 1200000,
